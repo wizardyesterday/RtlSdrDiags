5ecc925b2626ea43815b627d4ed6e7b14bf2226e
Chris G <chris@saturn.earth>
2023-08-03 21:35:48 -0500

    The main program, radioApp.cc now accepts command line arguments for the host IP address and the host port.  This establishes a path for streaming IQ data.

--- radioDiags/src_diags/radioApp.cc
+++ radioDiags/src_diags/radioApp.cc
@@ -20,6 +20,13 @@
 #define DEFAULT_HOST_IP_ADDRESS "192.93.16.87"
 #define DEFAULT_HOST_PORT (8001)
 
+// This structure is used to consolidate user parameters.
+struct MyParameters
+{
+  char *hostIpAddressPtr;
+  int *hostPortPtr;
+};
+
 extern bool diagUi_timeToExit;
 
 //_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/
@@ -67,7 +74,7 @@ FrequencySweeper *diagUi_frequencySweeperPtr;
 //
 // To run this program, type,
 //
-// ./radioApp hostIpAddress hostPort
+// ./radioApp -a hostIpAddress -p hostPort
 //
 //
 // Anyway, enjoy! Chris G.06/06/2017
@@ -103,26 +110,125 @@ static void processPcmData(int16_t *bufferPtr,uint32_t bufferLength)
 
 } // processPcmData
 
+/*****************************************************************************
+
+  Name: getUserArguments
+
+  Purpose: The purpose of this function is to retrieve the user arguments
+  that were passed to the program.  Any arguments that are specified are
+  set to reasonable default values.
+
+  Calling Sequence: exitProgram = getUserArguments(parameters)
+
+  Inputs:
+
+    parameters - A structure that contains pointers to the user parameters.
+
+  Outputs:
+
+    exitProgram - A flag that indicates whether or not the program should
+    be exited.  A value of true indicates to exit the program, and a value
+    of false indicates that the program should not be exited..
+
+*****************************************************************************/
+bool getUserArguments(int argc,char **argv,struct MyParameters parameters)
+{
+  bool exitProgram;
+  bool done;
+  int i;
+  int opt;
+
+  // Default not to exit program.
+  exitProgram = false;
+
+  //_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/
+  // Default parameters.
+  //_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/
+  // The host IP address.
+  strcpy(parameters.hostIpAddressPtr,DEFAULT_HOST_IP_ADDRESS);
+ 
+  // The host listener port.
+  *parameters.hostPortPtr = DEFAULT_HOST_PORT;
+  //_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/
+
+  // Set up for loop entry.
+  done = false;
+
+  //_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/
+  // Retrieve the command line arguments.
+  //_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/
+  while (!done)
+  {
+    // Retrieve the next option.
+    opt = getopt(argc,argv,"a:p:h");
+
+    switch (opt)
+    {
+      case 'a':
+      {
+        // Retrieve the IP address.        
+        strcpy(parameters.hostIpAddressPtr,optarg);
+        break;
+      } // case
+
+      case 'p':
+      {
+        // Retrieve the host listener port.
+        *parameters.hostPortPtr = atoi(optarg);
+        break;
+      } // case
+
+      case 'h':
+      {
+        // Display usage.
+        fprintf(stderr,"./radioDiags -a <hostIpAddress: x.x.x.x> "
+                "-p <hostPort>\n");
+
+        // Indicate that program must be exited.
+        exitProgram = true;
+        break;
+      } // case
+
+      case -1:
+      {
+        // All options consumed, so bail out.
+        done = true;
+      } // case
+
+    } // switch
+
+  } // while
+  //_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/
+
+  return (exitProgram);
+
+} // getUserArguments
+
 //***********************************************************************
 // Mainline code.
 //***********************************************************************
 int main(int argc,char **argv)
 {
   bool success;
+  bool exitProgram;
   uint64_t receiveFrequency;
   uint32_t sampleRate;
   struct timeval timeout;
-  char *hostIpAddress;
   int hostPort;
+  char hostIpAddress[32];
+  struct MyParameters parameters;
 
-  hostIpAddress = DEFAULT_HOST_IP_ADDRESS;
-  hostPort = DEFAULT_HOST_PORT;
+  // Set up for parameter transmission.
+  parameters.hostPortPtr = &hostPort;
+  parameters.hostIpAddressPtr = hostIpAddress;
 
-  if (argc == 3)
+  // Retrieve the system parameters.
+  exitProgram = getUserArguments(argc,argv,parameters);
+
+  if (exitProgram)
   {
-    // Retrieve IQ dump network parameters.  Yes, you can break this.
-    hostIpAddress = argv[1];
-    hostPort = atoi(argv[2]);
+    // Bail out.
+    return (0);
   } // if
 
   receiveFrequency = 162550000;
