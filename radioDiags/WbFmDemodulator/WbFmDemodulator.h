e0b36c1b71ec05d808aa7c02e883723adb49318f
Chris G <chris@saturn.earth>
2023-10-10 15:33:05 -0500

    A lowpass filter now precedes the wideband FM demodulator.  This should reduce noise, and there is a gain in adjacent channel selectivity.

--- radioDiags/WbFmDemodulator/WbFmDemodulator.h
+++ radioDiags/WbFmDemodulator/WbFmDemodulator.h
@@ -12,6 +12,7 @@
 #include <stdint.h>
 
 #include "IirFilter.h"
+#include "FirFilter_int16.h"
 #include "Decimator_int16.h"
 
 class WbFmDemodulator
@@ -53,6 +54,16 @@ class WbFmDemodulator
 
   // Demodulated data is converted into PCM data for listening.
   int16_t pcmData[512];
+
+  //_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/
+  // The first two decimators are used to lower the sample rate to 64000S/s
+  // that is useful for narrowband FM demodulation.  Note that we have
+  // tuner decimators for both, the I and the Q arm.  The third decimator
+  // lowers the sample rate to 16000S/s. The fourth decimator lowers the
+  // sample rate to that needed for 8000S/s PCM audio.
+  //_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/
+  FirFilter_int16 *iPreDemodFilterPtr;
+  FirFilter_int16 *qPreDemodFilterPtr;
 
   //_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/
   // These three decimators represent a three-stage decimator that
